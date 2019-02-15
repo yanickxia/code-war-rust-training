8ffb35f8f98ab6356458ad2412a405967c5fba2e
yann <me.yan.xia@qq.com>
2019-02-15 17:57:26 +0800

    Add more

---
+++ roman-numerals-encoder/src/lib.rs
@@ -0,0 +1,13 @@
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+
+
+    /// Converts a number to a string representating roman numeral.
+    fn num_as_roman(num: i32) -> String {
+        ...
+    }
+}
